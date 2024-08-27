3014cf2529a9c8259503da09751403d88c3ba10e
Alesssandra F R <102836095+Alle84fr@users.noreply.github.com>
2024-08-27 10:41:43 -0300

    Add files via upload

---
+++ aula27.py
@@ -0,0 +1,56 @@
+#classe define a estrutura do objeto ex: maternidade
+#construtor gera as instânias das classes, inicializar os tributos ex: sala do parto
+#valores/objetos possui valores específico , que está no projeto prioncipal ex: mome e peso da criança
+
+class Livro:
+    def _init_ (self, title, author, pages): #estou colocando os parâmetro, guia de valores a ser passado
+        self.titulo = title
+        self.autor = author
+        self.quatPag = pages
+
+# programa principal:
+livroA - Livro("Cavalot", "Jesus", 200)
+livroA - Livro("Eguanot", "Jesas", 2000)
+livroA - Livro("Potrancanot", "Jeses", 11)
+
+# cadastrar livros manualmente
+
+titu = input("nome livro: ")
+aut = input("nome autor: ")
+nupag = int(input("nome livro"))
+
+# para por ilimitaçõa, indicativo do type
+
+def _init_ (self, title:str, author:str, pages:int): #:coloca os types de cada variável
+        self.titulo = title
+        self.autor = author
+        self.quatPag = pages
+
+# para poder escrever em ordem aleatória, fora de ordem
+
+def _init_ (self, title:str, author:str, pages:int): #:coloca os types de cada variável
+        self.titulo = title
+        self.autor = author
+        self.quatPag = pages
+ livroc = Livro(title = titulo, author= autor, pages= quatPag)
+
+def metodo(self):
+      """
+      primeira linha com estas aspas triplas, diz o que faz esta função, depois começa a 
+      função/ parãmetro do construtor 
+      """
+      return None
+
+# iniciar todos os objetos com mesmo padrão ex: todos bebês tem 0 numeros de vida
+
+def _init_ (self, title:str, author:str, pages:int, mate:str = "dia"): #:coloca os types de cada variável
+        self.titulo = title
+        self.autor = author
+        self.quatPag = pages
+        self.material = mate
+ livrod = Livro(title = titulo, author= autor, pages= quatPag, materail) # se não definiu sai dia
+livrod = Livro(title = titulo, author= autor, pages= quatPag, materail=ano) # aqui mudei a favalor padrão
+
+print(livrod) # mostra o endereço de memória onde está armazenado e o type
+
+# para imprimir deve definir um método
