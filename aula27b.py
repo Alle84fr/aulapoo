94d7743d09ff96d08550c09311b60fc279c88148
Alesssandra F R <102836095+Alle84fr@users.noreply.github.com>
2024-08-27 11:14:01 -0300

    Add files via upload

---
+++ aula27b.py
@@ -0,0 +1,11 @@
+class Televi: # como aqui o valor inicia sempre com 0, não tem parametro, parametro é para decidir como iniciaria 
+    def __int__ (self):
+        self.volume = 0
+        self. canal = 0
+    def aumentarVol(self):
+        self.volume += 1
+    
+    #fazer a mesma coisa para aumentar os outros
+
+
+
---
+++ aula27bprin.py
@@ -0,0 +1,8 @@
+from televisao import Televisao
+
+tv = Televisao
+tv.alterarCana (5)
+
+# classe representa conjuntos de objetos/ entidades do mundo real que são associadas
+# o atribuito de um classe recebe parametro/ valor que recebe o parametro de outro objeto
+# ex: classe: carro, tem as instâncias como pneus, motor, que juntos forma o carro
